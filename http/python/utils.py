250311d3f41bd9d839b478c48ec7f5fcdfe38454
Sushant Taneja <sushant.taneja@snapdeal.com>
2015-08-16 21:50:17 +0530

    Fixes blob upload issue of production server

--- http/python/utils.py
+++ http/python/utils.py
@@ -2,11 +2,11 @@
 
 
 def encode_multipart_formdata(file_data, mime_type, upload_id):
-    boundary = 'paLp12Buasdasd40tcxAp97curasdaSt40bqweastfarcUNIQUE_STRING'
-    crlf = '\r\n'
+    boundary = 'DeviceImageUploadFormBoundary84BEAM87huhifeMEucwe87UPugnlopoSCOTTY'
+    crlf = '\n'
     line = []
     line.append('--' + boundary)
-    line.append('Content-Disposition: form-data; name="%s"; filename="%s" ' % ('file_info', str(upload_id)))
+    line.append('Content-Disposition: form-data; name="%s"; filename="%s"' % ('file_info', str(upload_id)))
     line.append('Content-Type: %s' % mime_type)
     line.append('')
     line.append(file_data)
